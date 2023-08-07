069e9abfec35f9a0c76f5a01388edff897d48c38
Dylan Cawsey <dylancawsey@gmail.com>
2023-08-07 20:20:45 +1000

    Fixed string types mismatch for splitting a string by delimiter

--- Main.hs
+++ Main.hs
@@ -1,9 +1,11 @@
-import System.Environment   
-import Data.List  
-import Data.Text (splitOn)
+{-# LANGUAGE OverloadedStrings #-}
+import System.Environment
+import Data.List
+import Data.Text (splitOn, pack)
 
-main = do  
+main = do
     args <- getArgs                  -- IO [String]
     let inputString = head args -- args !! <index>
-    let listOfWords = splitOn " " inputString
+    let inputText = pack inputString
+    let listOfWords = splitOn " " inputText
     print listOfWords
